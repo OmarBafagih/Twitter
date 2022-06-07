073c0f1e6cc03cc916f726be8f9de8a86f6b1902
Omar Bafagih <bafagihomar260@gmail.com>
2022-06-07 16:56:24 -0700

    Made network calls and created models
    Created User and Tweet models that will be used to load data from the attained Json object that will be returned from the Request(s)

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -9,12 +9,34 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import okhttp3.Headers;
+
 public class TimelineActivity extends AppCompatActivity {
+    TwitterClient client;
+    public static final String TAG = "TimelineActivity";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
+        client = TwitterApp.getRestClient(this);
+        populateHomeTimeline();
 
+    }
+
+    private void populateHomeTimeline() {
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG, "onSuccess");
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e(TAG, "OnFailure", throwable);
+            }
+        });
     }
 }
